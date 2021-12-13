d1fd2abbaab1289072e3a7c76898160135a203c4
memarino92 <memarino92@gmail.com>
2021-12-12 22:28:04 -0500

    added unleash

---
+++ server.js
@@ -0,0 +1,22 @@
+require('dotenv').config();
+const unleash = require('unleash-server');
+
+unleash
+  .start({
+    db: {
+      ssl: false,
+      host: process.env.DB_HOST,
+      port: process.env.DB_PORT,
+      database: process.env.DB_DATABASE,
+      user: process.env.DB_USER,
+      password: process.env.DB_PASSWORD,
+    },
+    server: {
+      port: process.env.SERVER_PORT,
+    },
+  })
+  .then((unleash) => {
+    console.log(
+      `Unleash started on http://localhost:${unleash.app.get('port')}`
+    );
+  });
